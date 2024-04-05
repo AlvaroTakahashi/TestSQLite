a8b4c83d440b5802f54dff2ab502226577784022
Alvaro Garciaguirre <garciaguirrealvaro@hotmail.com>
2024-04-04 20:20:25 -0600

    building

---
+++ WindowsFormsApp1/Class/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace WindowsFormsApp1.Class
+{
+    public class Conexion
+    {
+        private static string cadena = ConfigurationManager.ConnectionStrings["cadena"].ConnectionString;
+        
+            private static Conexion _instanci = null;
+
+        public Conexion()
+        {
+           
+        }
+
+        public static Conexion Instancia
+        {
+            get
+            {
+                if (_instanci == null)
+                {
+                    _instanci = new Conexion();
+                }
+                return _instanci;
+            }
+        }
+
+
+    }
+}
